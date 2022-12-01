5ed64d0d2ba82326f7d3a49dedb50a8a6eb2d6fc
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2022-12-01 03:55:57 +0000

    Working game selection system

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,62 @@
 <script>
-  import LanguageSelect from "./LanguageSelect.svelte"
-  import GameSelect from "./GameSelect.svelte"
+  import SelectMenu from "./SelectMenu.svelte"
   import GameEnglishAnimals from "./GameEnglishAnimals.svelte"
   import GameFrenchAnimals from "./GameFrenchAnimals.svelte"
   import GameFrenchIPAAnimals from "./GameFrenchIPAAnimals.svelte"
   import GameEnglishColors from "./GameEnglishColors.svelte"
+
+  let selectedLanguage = "en"
+  let selectedGame = "animals"
+  let languageMenu = [
+    {
+      code: "en",
+      name: "English",
+      games: [
+        {
+          code: "animals",
+          name: "Animals",
+          component: GameEnglishAnimals,
+        },
+        {
+          code: "colors",
+          name: "Colors",
+          component: GameEnglishColors,
+        },
+      ],
+    },
+    {
+      code: "fr",
+      name: "French",
+      games: [
+        {
+          code: "animals",
+          name: "Animaux",
+          component: GameFrenchAnimals,
+        },
+      ],
+    },
+    {
+      code: "fr-ipa",
+      name: "French IPA",
+      games: [
+        {
+          code: "animals",
+          name: "Animaux",
+          component: GameFrenchIPAAnimals,
+        },
+      ],
+    },
+  ]
+  $: gameMenu = languageMenu.find(o => o.code == selectedLanguage).games
+  $: defaultGame = gameMenu[0].code
+  $: {
+    if (!gameMenu.find(o => o.code == selectedGame)) {
+      selectedGame = defaultGame
+    }
+  }
+  $: gameComponent = gameMenu.find(o => o.code == selectedGame).component
 </script>
 
-<LanguageSelect />
-<GameSelect />
-<!-- <GameEnglishAnimals /> -->
-<!-- <GameFrenchAnimals /> -->
-<!-- <GameFrenchIPAAnimals /> -->
-<GameEnglishColors />
+<SelectMenu options={languageMenu} bind:selected={selectedLanguage} />
+<SelectMenu options={gameMenu} bind:selected={selectedGame} />
+<svelte:component this={gameComponent}/>
---
+++ src/SelectMenu.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let options
+  export let selected
+</script>
+
+<nav>
+  {#each options as option}
+    <div
+      class:selected={option.code == selected}
+      on:click={() => selected = option.code}
+    >{option.name}</div>
+  {/each}
+</nav>
+
+<style>
+  nav {
+    font-family: sans-serif;
+    display: flex;
+    gap: 5px;
+    background-color: #222;
+    color: white;
+    text-transform: uppercase;
+  }
+  nav > div {
+    padding: 4px;
+  }
+  .selected {
+    background-color: #0a6;
+  }
+</style>
